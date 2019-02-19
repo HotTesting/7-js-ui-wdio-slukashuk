1f8c49f3586bf4985c23135503bd50a8ebda109c
Svetlana <svetlana.lukashuk@wolterskluwer.com>
2019-02-19 20:58:43 +0200

    test

--- tests/test.ts
+++ tests/test.ts
@@ -3,5 +3,7 @@ describe('Protractor', function () {
     it('Should be alive', function () {
         browser.url('/')
         console.log('--Test passed!')
+        console.log('--Test passed!')
+
     })
 })
